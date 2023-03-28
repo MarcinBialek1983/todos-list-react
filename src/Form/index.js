5e040667fe2ac7da8b57fef882916f39892bcefd
Marcin Bialek <test-git@youcode.pl>
2023-03-28 15:09:00 +0200

    added empty task blok

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import "./style.css"
 
-const Form = ({addNewTask}) => {
-const [ newTaskContet, setNewTaskContent ] = useState("");
+const Form = ({ addNewTask }) => {
+    const [newTaskContet, setNewTaskContent] = useState("");
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
-    addNewTask(newTaskContet.trim());
-    setNewTaskContent("");
-}
+    const onFormSubmit = (event) => {
+        event.preventDefault(); 
+
+        const trimmedNewTaskContent = newTaskContet.trim();
+
+        if (!trimmedNewTaskContent) {
+            return;
+        }
+
+        addNewTask(trimmedNewTaskContent);
+        setNewTaskContent("");
+    }
     return (
-        <form 
-        className="form"
-        onSubmit={onFormSubmit}>
-            <input 
-            className="form__input" 
-            placeholder="Co jest do zrobienia?"
-            value={newTaskContet}
-            onChange={({target}) => setNewTaskContent(target.value)} />
+        <form
+            className="form"
+            onSubmit={onFormSubmit}>
+            <input
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                value={newTaskContet}
+                onChange={({ target }) => setNewTaskContent(target.value)} />
             <button className="form__button">Dodaj zadanie</button>
         </form>
     )
